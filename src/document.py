b07d923ee3c41d7aa99171aa8b2b1a25c2bc129c
Lost <pandagaminggr@gmail.com>
2024-06-16 17:42:52 +0300

    feat: implement tf, idf, tf_idf

--- src/document.py
+++ src/document.py
@@ -43,34 +43,8 @@ class Document:
                 self.tokens[p_token] = 1
         return self.tokens
 
-    def rank(self, M, d: float = 0.85) -> None:
-        """
-        Implements the PageRank webpage ranking algorithm over an array of files or pages.
-        More info: https://en.wikipedia.org/wiki/PageRank
-
-        Parameters
-        ----------
-        M : numpy array
-            adjacency matrix where M_i,j represents the link from 'j' to 'i', such that for all 'j'
-            sum(i, M_i,j) = 1
-        d : float, optional
-            damping factor, by default 0.85
-
-        Returns
-        -------
-        numpy array
-            a vector of ranks such that v_i is the i-th rank from [0, 1],
-
-        """
-
-        N = M.shape[1]
-        w = np.ones(N) / N
-        M_hat = d * M
-        v = M_hat @ w + (1 - d)
-        while np.linalg.norm(w - v) >= 1e-10:
-            w = v
-            v = M_hat @ w + (1 - d)
-        return v
+    def rank(self, files) -> None:
+        pass
 
     def read_file(self, file) -> list[str]:
         """
---
+++ src/td_idf.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+class Rank:
+    def term_frequency(self, word, file) -> float:
+        return np.log10(1 + file.count(word))
+
+    def inverse_document_frequency(self, word, num_of_files) -> float:
+        count_of_files = len(num_of_files) + 1
+        count_of_fils_with_word = sum([1 for doc in num_of_files if word in doc]) + 1
+        return np.log10(count_of_files / count_of_fils_with_word) + 1
+
+    def tf_idf(self, word, file, num_of_files) -> float:
+        tf = self.term_frequency
+        idf = self.inverse_document_frequency
+
+        return tf(word, file) * idf(word, num_of_files)
